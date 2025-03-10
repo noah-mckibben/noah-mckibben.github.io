8ffce339232ad2397fc4d6b9077cf388be9962a3
Noah Mckibben <180349465+noah-mckibben@users.noreply.github.com>
2025-03-10 16:26:43 -0400

    background updates

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ const button = nav.querySelector('button');
 const project = document.querySelector('a.project')
 const about = document.querySelector('a.about')
 const home = document.querySelector('a.home')
+const spanhome = document.querySelector('span.home')
+const spanabout = document.querySelector('span.about')
+const spanproject = document.querySelector('span.project')
 
 darkToggle.addEventListener('click', () => {
     nav.classList.toggle('darkNav')
@@ -11,7 +14,28 @@ darkToggle.addEventListener('click', () => {
     project.classList.toggle('darkText');
     about.classList.toggle('darkText');
     home.classList.toggle('darkText');
+    spanhome.classList.toggle('darkText');
+    spanabout.classList.toggle('darkText');
+    spanproject.classList.toggle('darkText');
+
 })
+
+
+function background() {
+    VANTA.WAVES({
+        el: "html",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: true,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x70704,
+        waveHeight: 23.50,
+        zoom: 0.97
+    })
+}
 
 
 function hideAllContentDivs(){
